9b9a6daf46e6861cdab82ce1f396fd97d166d916
franco sanchez <franco_genta@hotmail.com>
2022-08-26 17:20:10 -0300

    feat: add .vscode folder

--- src/model/books.ts
+++ src/model/books.ts
@@ -7,30 +7,38 @@ export interface BooksDocument extends pkg.Document {
   author: string;
   category: string;
   publicationDate: string;
+  sourceLink: string;
   numberPages: string;
 }
 
 const booksSchema = new Schema({
   title: {
     type: String,
-    // required: true,
+    required: true,
   },
   description: {
     type: String,
-    // required: true,
+    required: true,
   },
   author: {
     type: String,
-    // required: true
+    required: true
   },
   category: {
     type: String,
+    required: true
+  },
+  sourceLink: {
+    type: String,
+    required: true
   },
   publicationDate: {
     type: String,
+    required: true
   },
   numberPages: {
     type: Number,
+    required: true
   }
   // imgUrl: {
   //   type: Buffer,
@@ -39,9 +47,9 @@ const booksSchema = new Schema({
 
 booksSchema.set('toJSON', {
   transform: (_, returnedObject) => {
-    returnedObject.id = returnedObject._id
-    delete returnedObject._v
-    delete returnedObject._id
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._v;
+    delete returnedObject._id;
   }
 });
 
