6970ac5f8e9b7a03e39e8ce4c46592f3d319ab6b
franco sanchez <franco_genta@hotmail.com>
2023-01-30 13:15:36 -0300

    fix: 404 error x2

--- src/server.ts
+++ src/server.ts
@@ -85,12 +85,12 @@ app.get('/', (req, res: Response) => {
   `);
 });
 app.use('/api', books);
-// app.all('*', (req, res: Response) => res.status(404).json({ error: 'Not found' }));
 app.use(Sentry.Handlers.errorHandler());
 app.use(function onError(err: ErrorRequestHandler, req: Request, res: any, next: NextFunction) {
   res.statusCode = 500;
   res.end(res.sentry + "\n");
 });
+app.all('*', (req, res: Response) => res.status(404).json({ error: 'Not found' }));
 
 // app.use(cookieSession({
 //   name: 'session',
