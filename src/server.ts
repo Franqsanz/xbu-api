2665898b21ba85cd9a41ca4e13fa2f48109c73fe
franco sanchez <franco_genta@hotmail.com>
2023-10-27 12:42:14 -0300

    chore: testing data limit increase

--- src/server.ts
+++ src/server.ts
@@ -43,7 +43,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(compression());
 app.use(cookieParser());
