5df48507257307c68b7bbcd7454faa4b2ab48abe
franco sanchez <franco_genta@hotmail.com>
2024-03-01 13:45:20 -0300

    chore: now in the profile the results are paginated

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -486,18 +486,46 @@ async function getCheckUser(req: Request, res: Response) {
 async function getUserAndBooks(req: Request, res: Response) {
   try {
     const { username } = req.params;
+    const limit = parseInt(req.query.limit as string);
+    const page = parseInt(req.query.page as string) || 1;
+    const offset = (page - 1) * limit;
+
     const user = await usersModel.findOne({ username: username }, 'uid name picture createdAt');
 
     if (!user) {
       return res.status(404).json({ error: { message: 'Usuario no encontrado' } });
     }
 
-    // Obtener libros del usuario por su ID
-    const books = await booksModel.find({ userId: user.uid }, 'title category language authors pathUrl image').sort({ _id: -1 });
+    const results = await booksModel.find({ userId: user.uid }, 'title category language authors pathUrl image').skip(offset).limit(limit).sort({ _id: -1 }).exec();
+    const totalBooks = await booksModel.countDocuments({ userId: user.uid });
 
-    return res.status(200).json({ user, books });
+    const totalPages = Math.ceil(totalBooks / limit);
+    const nextPage = page < totalPages ? page + 1 : null;
+    const nextPageLink = nextPage ? `${req.protocol}://${req.get('host')}/api/users${req.path}?page=${nextPage}${limit ? `&limit=${limit}` : ''}` : null;
+
+    const info = {
+      totalBooks,
+      totalPages,
+      currentPage: page,
+      nextPage: nextPage,
+      prevPage: page > 1 ? page - 1 : null,
+      nextPageLink: nextPageLink,
+      prevPageLink: page > 1 ? `${req.protocol}://${req.get('host')}/api/users${req.path}?page=${page - 1}${limit ? `&limit=${limit}` : ''}` : null,
+    };
+
+    const response = {
+      info,
+      user,
+      results,
+    };
+
+    if (results.length < 1) {
+      return res.status(404).json({ info: { message: 'No se encontraron más libros' } });
+    }
+
+    return res.status(200).json(response);
   } catch (error) {
-    console.error('Error al obtener usuario y libros:', error);
+    console.error('Error al obtener el usuario y los libros:', error);
   }
 }
 
